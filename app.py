a96e42105faf8fd6692b0b0569856205a8eb09f4
Aman Kumar <137410641+kumar-amann@users.noreply.github.com>
2025-08-18 00:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -141,4 +141,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
